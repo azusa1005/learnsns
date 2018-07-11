e2a5ca8050a50ba20f0ed442102e313155517a83
Azusa <azusa.i.1005@gmail.com>
2018-07-11 10:30:48 +0800

    データベースと連携

--- bbs_no_css.php
+++ bbs_no_css.php
@@ -1,7 +1,7 @@
 <?php
   // ここにDBに登録する処理を記述する
 
-
+if(!empty($_POST)){
 $nickname = htmlspecialchars($_POST['nickname']);
 $comment = htmlspecialchars($_POST['comment']);
 
@@ -13,16 +13,20 @@ $dbh = new PDO($dsn, $user, $password);
 $dbh->query('SET NAMES utf8');
 
 // ２．SQL文を実行する
-$sql = 'INSERT INTO `posts`( `nickname`, `comment`) VALUES (?,?)';
+$sql = 'INSERT INTO `posts`( `nickname`, `comment`,`created`) VALUES (?,?,?)';
 
-$data=array($nickname,$comment);
+$data=array($nickname,$comment, date("Y-m-d H:i:s"));
 $stmt = $dbh->prepare($sql);
 $stmt->execute($data);
+
+}
 
 // ３．データベースを切断する
 $dbh = null;
 
 ?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -37,6 +41,47 @@ $dbh = null;
     </form>
     <!-- ここにニックネーム、つぶやいた内容、日付を表示する -->
 
+<?php 
+
+$dsn = 'mysql:dbname=online_bbs;host=localhost';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn, $user, $password);
+$dbh->query('SET NAMES utf8');
+
+//データベースにつなげる処理は重いから最初に一回やって、
+//最後にnullで切れば良い。
+//htmlよりphpのが強い　どこで区切られていてもphpの処理がされてからhtmlが処理される
+
+$sql = 'SELECT * FROM `posts` ORDER BY created DESC';
+
+//DESK→最新の投稿から表示される。ASC 古いものから表示　
+//どちらも書かない場合はASCになる
+//created 以外にnickname とかだとアイウエオ順、年齢だと若い順とか
+
+$stmt = $dbh->prepare($sql);
+$stmt->execute();
+
+while(1){
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($rec == false){
+        break;
+    }
+
+echo $rec['nickname'] . '<br>';
+echo $rec['comment'] . '<br>';
+echo $rec['created'] . '<br>';
+echo '<hr>';
+
+}
+
+$dbh = null;
+
+?>
+
+
 
 </body>
 </html>
+
+
